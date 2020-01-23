5b19ed774b2d4ea4b1a22340c10fc987e1704eac
erocknine <echeungsk@gmail.com>
2020-01-23 19:11:25 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,7 +12,7 @@ end
 def print_first_directors_movie_titles
   i = 0
   movies = directors_database[0][:movies]
-  while i < movies.count do
+  while i < movies.length do
     puts movies[i][:title]
     i += 1
   end
